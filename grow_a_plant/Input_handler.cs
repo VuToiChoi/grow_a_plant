ff2f4b3ff93c0dfa6215529a03e2d6988d6859a2
Vespri <vudanghuy255@yahoo.com>
2026-01-26 14:04:04 +0100

    All necessary classes created.

---
+++ grow_a_plant/Input_handler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grow_a_plant
+{
+    internal class Input_handler
+    {
+    }
+}
