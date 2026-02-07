c4cebae48d4e08cba10f2987adac138732f33ac5
Yeeunderscore <olle.bjarang@gmail.com>
2026-02-07 10:22:20 +0100

    Made it possible for the input handler to return one of the following pressed keys: up, down, left, right, z, x.

--- grow_a_plant/Input_handler.cs
+++ grow_a_plant/Input_handler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,56 @@ using System.Threading.Tasks;
 
 namespace grow_a_plant
 {
-    internal class Input_handler
+    public class Input_handler
     {
-        // keyboard_state, get pressed key
+        KeyboardState _keyboard_state;
+
+        public enum key
+        {
+            up,
+            down,
+            left,
+            right,
+            z,
+            x,
+            none
+        }
+
+        public Input_handler(KeyboardState keyboard_state)
+        {
+            _keyboard_state = keyboard_state;
+        }
+
+        public key get_pressed_key()
+        {
+            if (_keyboard_state.IsKeyDown(Keys.Up))
+            {
+                return key.up;
+            }
+            else if (_keyboard_state.IsKeyDown(Keys.Down))
+            {
+                return key.down;
+            }
+            else if (_keyboard_state.IsKeyDown(Keys.Left))
+            {
+                return key.left;
+            }
+            else if (_keyboard_state.IsKeyDown(Keys.Right))
+            {
+                return key.right;
+            }
+            else if (_keyboard_state.IsKeyDown(Keys.Z))
+            {
+                return key.z;
+            }
+            else if (_keyboard_state.IsKeyDown(Keys.X))
+            {
+                return key.x;
+            }
+            else
+            {
+                return key.none;
+            }
+        }
     }
 }
