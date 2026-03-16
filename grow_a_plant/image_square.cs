37234c434325a3dc64e37126399be97febf23d1e
Yeeunderscore <olle.bjarang@gmail.com>
2026-03-16 13:08:31 +0100

    added the Image_square class and its fields

---
+++ grow_a_plant/image_square.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grow_a_plant
+{
+    class Image_square
+    {
+        public int X_position { get; private set; }
+
+        public int Y_position { get; private set; }
+
+        public Texture2D Image { get; private set; }
+    }
+}
