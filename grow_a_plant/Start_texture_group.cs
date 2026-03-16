6acf5c4c1efa3f00f0c2927c28f88a61286b039c
Yeeunderscore <olle.bjarang@gmail.com>
2026-03-16 13:37:21 +0100

    added the abstract class Texture_group and (currently) the only two classes that inherit this class, Start_texture_group and Settings_texture_group

---
+++ grow_a_plant/Start_texture_group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grow_a_plant
+{
+    class Start_texture_group : Texture_group
+    {
+        public Start_texture_group() : base() 
+        { 
+            Image_squares.Add(new Image_square() { X_position = 0, Y_position = 0, Image = Game1.Water_image }); // add something like this
+        }
+    }
+}
---
+++ grow_a_plant/Texture_group.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grow_a_plant
+{
+    abstract class Texture_group
+    {
+        public List<Image_square> Image_squares { get; private set; }
+
+        // will probably need more fields in the future, like animations and text
+
+        public Texture_group()
+        {
+            Image_squares = new List<Image_square>();
+        }
+    }
+}
