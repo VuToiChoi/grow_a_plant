2e064c4da68e8d8e1e22aeb33f7a2802c1b179a1
Yeeunderscore <olle.bjarang@gmail.com>
2026-04-19 20:20:56 +0200

    created the Text_rectangle class and updated the Text_handler to use the Button_command_package. Text_handler's method of operating was changed. Text_handler was also renamed to Information_text_updater.

---
+++ grow_a_plant/Text_rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grow_a_plant
+{
+    class Text_rectangle
+    {
+        public string Text {  get; set; }
+
+        public int X_postition { get; private set; }
+
+        public int Y_postition { get; private set; }
+
+        public Text_rectangle(string text, int x_postition, int y_postition)
+        {
+            Text = text;
+            X_postition = x_postition;
+            Y_postition = y_postition;
+        }
+    }
+}
