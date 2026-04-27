cee2ca963346672edc1d0ae0cbf5aa6d59759877
fabiancarlsson07-rgb <fabiancarlsson07@gmail.com>
2026-04-27 14:09:02 +0200

    music and sound effect completed

--- grow_a_plant/Sound_handler.cs
+++ grow_a_plant/Sound_handler.cs
@@ -1,24 +1,45 @@
-﻿
     using System;
     using System.IO;
     using Microsoft.Xna.Framework.Content;
     using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
 
 namespace grow_a_plant
 {
     internal class Sound_handler
     {
-        private Song song;
+        private Song _song;
+        private ContentManager _content;
 
         public Sound_handler(ContentManager content)
         {
+            _content = content;
             try
             {
                 var audioPath = Path.Combine(content.RootDirectory, "music", "Musicforplant.mp3");
                 var fullPath = Path.GetFullPath(audioPath);
-                song = Song.FromUri("main_menu", new Uri(fullPath));
-                MediaPlayer.Play(song);
+                _song = Song.FromUri("main_menu", new Uri(fullPath));
+                MediaPlayer.Play(_song);
                 MediaPlayer.IsRepeating = true;
+                MediaPlayer.Volume = 0.5f;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Failed to load song: " + ex);
+                // continue without crashing
+            }
+        }
+
+        public void Water_Sound(ContentManager content) {
+
+            try
+            {
+                var audioPath = Path.Combine(content.RootDirectory, "music", "water.mp3");
+                var fullPath = Path.GetFullPath(audioPath);
+                _song = Song.FromUri("water", new Uri(fullPath));
+                MediaPlayer.Play(_song);
+                MediaPlayer.IsRepeating = true;
+                MediaPlayer.Volume = 1.0f;
             }
             catch (Exception ex)
             {
