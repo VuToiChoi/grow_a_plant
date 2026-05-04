ce3ecc206c8ca47003e4ce2be18f5c1c6e7ddeac
Yeeunderscore <olle.bjarang@gmail.com>
2026-05-04 13:46:19 +0200

    added _user_interface_interface to main_handler

--- grow_a_plant/Main_handler.cs
+++ grow_a_plant/Main_handler.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace grow_a_plant
 {
@@ -13,15 +16,27 @@ namespace grow_a_plant
 
         Plant_handler _plant_handler;
 
-        User_interface_handler _user_interface_handler;
+        User_interface_interface _user_interface_interface;
 
-        public Main_handler()
+        Dictionary<Menu_command_package.command_type, Button_command_package> _menu_command_package_command_type_to_button_command_package_dictionary = new Dictionary<Menu_command_package.command_type, Button_command_package> 
         {
-            _menu_handler_inerface = new Menu_handler_interface(); // fix
+            { Menu_command_package.command_type.water, new Button_command_package(Button_command_package.command_type.water) },
+            { Menu_command_package.command_type.open_log, new Button_command_package(Button_command_package.command_type.log) },
+            { Menu_command_package.command_type.fertelize, new Button_command_package(Button_command_package.command_type.fertilize) },
+            { Menu_command_package.command_type.open_settings, new Button_command_package(Button_command_package.command_type.settings) },
+            { Menu_command_package.command_type.return_to_game, new Button_command_package(Button_command_package.command_type.return_to_game) },
+            { Menu_command_package.command_type.save_game, new Button_command_package(Button_command_package.command_type.save_game) },
+            { Menu_command_package.command_type.load_game, new Button_command_package(Button_command_package.command_type.load_game) },
+            { Menu_command_package.command_type.exit_game, new Button_command_package(Button_command_package.command_type.exit_game) }
+        };
+
+        public Main_handler(SpriteBatch sprite_batch, SpriteFont sprite_font, GraphicsDevice graphics_device, ContentManager content)
+        {
+            _menu_handler_inerface = new Menu_handler_interface();
 
             _plant_handler = new Plant_handler(); // fix
 
-            _user_interface_handler = new User_interface_handler(); // fix
+            _user_interface_interface = new User_interface_interface(sprite_batch, sprite_font, graphics_device, content);
         }
 
         public void update()
@@ -33,9 +48,11 @@ namespace grow_a_plant
                 conduct_action(menu_handler_information.Selected_button.Command);
             }
 
-            _plant_handler.update(); // fix
+            // _plant_handler.update(); // fix
 
-            _interface_handler.update(); // fix
+            Texture_screen_information texture_screen_information = new Texture_screen_information(_menu_command_package_command_type_to_button_command_package_dictionary[menu_handler_information.Selected_button.Command], menu_handler_information.Button_is_pressed, 0.5f, 0.5f, 3, 0);
+
+            _user_interface_interface.update(texture_screen_information);
         }
 
         private void conduct_action(Menu_command_package.command_type command)
