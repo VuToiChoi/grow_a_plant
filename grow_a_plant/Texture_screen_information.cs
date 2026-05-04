2a73addea340cef00572dace47481ee9b3dbe4b2
Yeeunderscore <olle.bjarang@gmail.com>
2026-05-04 12:25:05 +0200

    added the property Time _of_the_day_in_seconds to Texture_screen_information

--- grow_a_plant/Texture_screen_information.cs
+++ grow_a_plant/Texture_screen_information.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,14 +18,17 @@ namespace grow_a_plant
         public float Fertilize_level { get; private set; }
 
         public int Growth_stage { get; private set; }
+
+        public int Time_of_the_day_in_seconds { get; private set; }
         
-        public Texture_screen_information(Button_command_package selected_button, bool button_is_pressed, float water_level, float fertilize_level, int growth_stage)
+        public Texture_screen_information(Button_command_package selected_button, bool button_is_pressed, float water_level, float fertilize_level, int growth_stage, int time_of_the_day_in_seconds)
         {
             Selected_button = selected_button;
             Button_is_pressed = button_is_pressed;
             Water_level = water_level;
             Fertilize_level = fertilize_level;
             Growth_stage = growth_stage;
+            Time_of_the_day_in_seconds = time_of_the_day_in_seconds;
         }
     }
 }
