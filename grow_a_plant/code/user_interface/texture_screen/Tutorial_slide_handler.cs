cb730a681ea5d2bc31b453639f2a96c8167cb843
Yeeunderscore <olle.bjarang@gmail.com>
2026-05-19 14:00:43 +0200

    Changed a name in Tutorial_slide_handler

--- grow_a_plant/code/user_interface/texture_screen/Tutorial_slide_handler.cs
+++ grow_a_plant/code/user_interface/texture_screen/Tutorial_slide_handler.cs
@@ -8,18 +8,18 @@ namespace grow_a_plant.code.user_interface.texture_screen
 {
     public class Tutorial_slide_handler
     {
-        private List<tutorial_slide> _tutorial_slides;
+        private List<Tutorial_slide> _tutorial_slides;
 
         public int current_slide_index { get; private set; }
 
         public Tutorial_slide_handler()
         {
-            _tutorial_slides = new List<tutorial_slide>
+            _tutorial_slides = new List<Tutorial_slide>
             {
-                new tutorial_slide("Welcome to Grow a Plant!\nIn this game, you navigate through menus with the WASD buttons or with the arrow buttons. When you have navigated to the correct button you press z to select it.\n\nTry it out:"),
-                new tutorial_slide("Your goal is to take care of a plant. You therefore have to water and fertilize it. After a while it might grow into another growth level.\nKeep in mind though, watering and fertilizing the plant too much or too little won't do. You have do it just perfectly."),
-                new tutorial_slide("The water and fertilize level is visible in two bars to the left. Depending on the weather outside, the water could be depleeted slower or quicker. The fertilize level depleetes when the plant grows."),
-                new tutorial_slide("Even though the game is exited, the plant will still grow. So you have to enter the game from time to time just to make sure it is doing okay.\n\nThe game saves automaticalley when exited. So no need to worry about that (even though those buttons exist in the settings).")
+                new Tutorial_slide("Welcome to Grow a Plant!\nIn this game, you navigate through menus with the WASD buttons or with the arrow buttons. When you have navigated to the correct button you press z to select it.\n\nTry it out:"),
+                new Tutorial_slide("Your goal is to take care of a plant. You therefore have to water and fertilize it. After a while it might grow into another growth level.\nKeep in mind though, watering and fertilizing the plant too much or too little won't do. You have do it just perfectly."),
+                new Tutorial_slide("The water and fertilize level is visible in two bars to the left. Depending on the weather outside, the water could be depleeted slower or quicker. The fertilize level depleetes when the plant grows."),
+                new Tutorial_slide("Even though the game is exited, the plant will still grow. So you have to enter the game from time to time just to make sure it is doing okay.\n\nThe game saves automaticalley when exited. So no need to worry about that (even though those buttons exist in the settings).")
             };
 
             current_slide_index = 0;
