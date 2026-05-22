c91e2ad0444e7a2813ec1fee174712a3f3b0feaf
Yeeunderscore <olle.bjarang@gmail.com>
2026-05-22 08:29:12 +0200

    added all the correct positions to the elements in Tutorial_texture_screen. Corrected some of the text in Tutorial_slide_handler

--- grow_a_plant/code/user_interface/texture_screen/Tutorial_slide_handler.cs
+++ grow_a_plant/code/user_interface/texture_screen/Tutorial_slide_handler.cs
@@ -16,10 +16,10 @@ namespace grow_a_plant.code.user_interface.texture_screen
         {
             _tutorial_slides = new List<Tutorial_slide>
             {
-                new Tutorial_slide("Welcome to Grow a Plant!\nIn this game, you navigate through menus with the WASD buttons or with the arrow buttons. When you have navigated to the correct button you press z to select it.\n\nTry it out:"),
-                new Tutorial_slide("Your goal is to take care of a plant. You therefore have to water and fertilize it. After a while it might grow into another growth level.\nKeep in mind though, watering and fertilizing the plant too much or too little won't do. You have do it just perfectly."),
-                new Tutorial_slide("The water and fertilize level is visible in two bars to the left. Depending on the weather outside, the water could be depleeted slower or quicker. The fertilize level depleetes when the plant grows."),
-                new Tutorial_slide("Even though the game is exited, the plant will still grow. So you have to enter the game from time to time just to make sure it is doing okay.\n\nThe game saves automaticalley when exited. So no need to worry about that (even though those buttons exist in the settings).")
+                new Tutorial_slide("Welcome to Grow a Plant!\n\nIn this game you navigate through menus with the WASD buttons\nor with the arrow buttons. When you have navigated to the correct\nbutton you press z to select it.\n\nTry it out:"),
+                new Tutorial_slide("Your goal is to take care of a plant.\n\nYou therefore have to water and fertilize it. After a while it might grow\ninto another growth level.\n\nKeep in mind though: watering and fertilizing the plant too much or too\nlittle won't do. If you want your plant to be at its best you have do it just\nperfectly."),
+                new Tutorial_slide("The water and fertilize level is visible in two bars on the left.\n\nDepending on the weather outside the water could be\ndepleeted slower or quicker. The fertilize level depleetes\nwhen the plant grows.\n\nYour starter plant thrives at a medium water level and a\nmedium fertilize level."),
+                new Tutorial_slide("Even though the game is exited the plant will still grow. So\nyou have to enter the game from time to time just to make \nsure it is doing okay.\n\nThe game saves automaticalley when exited. So no need to\nworry about that (even though those buttons exist in the settings).")
             };
 
             current_slide_index = 0;
@@ -41,13 +41,13 @@ namespace grow_a_plant.code.user_interface.texture_screen
 
         private void nudge_current_slide_index_into_valid_value()
         {
-            if (current_slide_index < 0) // sets the current slide index to the last slide if it is smaller than 0
-            {
-                current_slide_index = _tutorial_slides.Count - 1;
-            }
-            else if (current_slide_index >= _tutorial_slides.Count) // sets the current slide index to the first slide if it is bigger than the amount of slides
+            if (current_slide_index < 0) // sets the current_slide_index to zero if becomes smaller than zero
             {
                 current_slide_index = 0;
+            }
+            else if (current_slide_index >= _tutorial_slides.Count) // sets the current slide index to the last slide if it is bigger than the amount of slides
+            {
+                current_slide_index = _tutorial_slides.Count - 1;
             }
         }
 
--- grow_a_plant/code/user_interface/texture_screen/Tutorial_texture_screen.cs
+++ grow_a_plant/code/user_interface/texture_screen/Tutorial_texture_screen.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,40 +22,58 @@ namespace grow_a_plant.code.user_interface.texture_screen
 
             // background
             Texture_groups.Add("background", new Texture_group(0, 0, true));
-            Texture_groups["background"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\tutorialbackground")));
+            Texture_groups["background"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height), _content.Load<Texture2D>("picture's\\tutorialbackground")));
 
-
-            // information text
-            Texture_groups.Add("information_text", new Texture_group(0, 0, true));
-            Texture_groups["information_text"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.05), (int)(_screen_height * 0.05), (int)(_screen_width * 0.9), (int)(_screen_height * 0.65)));
-            Texture_groups["information_text"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.05), (int)(_screen_height * 0.05), (int)(_screen_width * 0.9), (int)(_screen_height * 0.65)));
-            Texture_groups["information_text"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.05), (int)(_screen_height * 0.05), (int)(_screen_width * 0.9), (int)(_screen_height * 0.65)));
+            // text
+            Texture_groups.Add("text", new Texture_group(0, 0, true));
+            Texture_groups["text"].Text_rectangles.Add(new Text_rectangle("Tutorial", (int)(_screen_width * 0.410), (int)(_screen_height * 0.070), (int)(_screen_width * 0.180), (int)(_screen_height * 0.120)));
+            Texture_groups["text"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.100), (int)(_screen_height * 0.190), (int)(_screen_width * 0.800), (int)(_screen_height * 0.450)));
 
 
             // slide counter
             Texture_groups.Add("slide_counter", new Texture_group(0, 0, true));
-            Texture_groups["information_text"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.05), (int)(_screen_height * 0.05), (int)(_screen_width * 0.9), (int)(_screen_height * 0.65)));
+            Texture_groups["slide_counter"].Text_rectangles.Add(new Text_rectangle("", (int)(_screen_width * 0.907), (int)(_screen_height * 0.650), (int)(_screen_width * 0.120), (int)(_screen_height * 0.075)));
+            update_slide_counter();
 
 
             // menu
+            int menu_width = _screen_width;
+            int menu_height = (int)(_screen_height * 0.234);
 
             //  next tutorial slide button selected
-            Texture_groups.Add("menu_previous_tutorial_slide_selected", new Texture_group(0, 0, true));
-            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
-            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
-            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups.Add("menu_previous_tutorial_slide_selected", new Texture_group(0, (int)(_screen_height * 0.766), true));
+            // images
+            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.080), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
+            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.378), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups["menu_previous_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.677), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            // text
+            Texture_groups["menu_previous_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("previous slide", (int)(menu_width * 0.110), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_previous_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("next slide", (int)(menu_width * 0.440), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_previous_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("just let me play!", (int)(menu_width * 0.700), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
 
             //  next tutorial slide button selected
-            Texture_groups.Add("menu_next_tutorial_slide_selected", new Texture_group(0, 0, true));
-            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
-            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
-            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups.Add("menu_next_tutorial_slide_selected", new Texture_group(0, (int)(_screen_height * 0.766), true));
+            // images
+            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.080), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.378), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
+            Texture_groups["menu_next_tutorial_slide_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.677), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            // text
+            Texture_groups["menu_next_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("previous slide", (int)(menu_width * 0.110), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_next_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("next slide", (int)(menu_width * 0.440), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_next_tutorial_slide_selected"].Text_rectangles.Add(new Text_rectangle("just let me play!", (int)(menu_width * 0.700), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
 
             //  exit tutorial button selected
-            Texture_groups.Add("menu_exit_tutorial_selected", new Texture_group(0, 0, true));
-            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
-            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebutton")));
-            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle(0, 0, _screen_width, (int)(_screen_height * 0.765), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
+            Texture_groups.Add("menu_exit_tutorial_selected", new Texture_group(0, (int)(_screen_height * 0.766), true));
+            // images
+            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.080), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.378), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebutton")));
+            Texture_groups["menu_exit_tutorial_selected"].Image_rectangles.Add(new Image_rectangle((int)(menu_width * 0.677), (int)(menu_height * 0.290), (int)(menu_width * 0.243), (int)(menu_height * 0.418), _content.Load<Texture2D>("picture's\\startmenuebuttonselect")));
+            // text
+            Texture_groups["menu_exit_tutorial_selected"].Text_rectangles.Add(new Text_rectangle("previous slide", (int)(menu_width * 0.110), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_exit_tutorial_selected"].Text_rectangles.Add(new Text_rectangle("next slide", (int)(menu_width * 0.440), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+            Texture_groups["menu_exit_tutorial_selected"].Text_rectangles.Add(new Text_rectangle("just let me play!", (int)(menu_width * 0.700), (int)(menu_height * 0.370), (int)(menu_width * 0.450), (int)(menu_height * 0.250)));
+
+            update_information_text();
         }
 
         public override void update(Texture_screen_information texture_screen_information)
@@ -95,14 +114,14 @@ namespace grow_a_plant.code.user_interface.texture_screen
             {
                 _tutorial_slide_handler.previous_slide();
                 update_slide_counter();
+                update_information_text();
             }
             else if (pressed_button_command_package.Command == Button_command_package.command_type.next_tutorial_slide)
             {
                 _tutorial_slide_handler.next_slide();
                 update_slide_counter();
+                update_information_text();
             }
-
-            update_information_text();
         }
 
         private void update_slide_counter()
@@ -112,7 +131,7 @@ namespace grow_a_plant.code.user_interface.texture_screen
 
         private void update_information_text()
         {
-            Texture_groups["information_text"].Text_rectangles[0].Text = _tutorial_slide_handler.get_current_slide_information_text();
+            Texture_groups["text"].Text_rectangles[1].Text = _tutorial_slide_handler.get_current_slide_information_text();
         }
     }
 }
